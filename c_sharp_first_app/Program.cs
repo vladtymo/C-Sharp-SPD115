d9691e78a326bfde7ed35cff2bc8fe723a035a6c
Vlad Tymoshchuk <tymo.vlad@gmail.com>
2022-10-01 17:42:58 +0300

    Add project files.

---
+++ c_sharp_first_app/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace c_sharp_first_app
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // -=-=-=-=-=-=-=-=- Console Input/Output -=-=-=-=-=-=-=-=-
+
+            Console.WriteLine("Hello!"); // with end line
+            Console.WriteLine("Hello!"); // without end line
+
+            // snippet: cw + TAB = Console.WriteLine();
+            Console.WriteLine();
+
+            
+            Console.WriteLine("\n\t\'!!!\'"); // with escape
+            Console.WriteLine(@"\n\t\'!!!\'"); // without escape
+
+            Console.Write("Enter your name: ");
+            string name = Console.ReadLine();
+
+            Console.WriteLine("Hello, dear " + name + " !!!");
+            // interpolation: $"...{expression}..."
+            Console.WriteLine($"Hello, dear {name} !!!");
+
+            int a = 5, b = 10;
+            Console.WriteLine($"{a} + {b} = {a + b}");
+
+            // parsing: converting from string to type
+
+            Console.Write("Enter current temperature in °C: ");
+            float temperature = float.Parse(Console.ReadLine());
+
+            Console.WriteLine($"{temperature}°C = {(temperature * 9/5) + 32}°F");
+        }
+    }
+}
